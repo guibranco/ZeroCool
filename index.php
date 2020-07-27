7b6d42d0a33026014ccd29d14ca986c334f6319f
Guilherme Branco Stracini <guilherme@guilhermebranco.com.br>
2020-07-27 17:00:24 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,9 @@
 <?php
-exit(header("Location: https://guibranco.github.io/?utm_campaign=custom_domain&utm_media=redirect&utm_source=" . $_SERVER["HTTP_HOST"], true, 301));
+
+$campaign = "custom_domain";
+
+if(isset($_SERVER["HTTP_REFERER"]))
+    $campaign = urlencode($_SERVER["HTTP_REFERER"]);
+
+exit(header("Location: https://guibranco.github.io/?utm_campaign=" . $campaign . "&utm_media=redirect&utm_source=" . $_SERVER["HTTP_HOST"], true, 301));
 ?>
