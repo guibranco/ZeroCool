0c853c9dbd52a4348e62f1c0c8ceb443883750d2
Guilherme Branco Stracini <guilherme@guilhermebranco.com.br>
2021-01-29 11:08:36 -0300

    Create index.php

---
+++ saldoBs2/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Push Saldo</title>
+  </head>
+
+  <body>
+    Saldo
+  </body>
+  <script src="https://cdn.onesignal.com/sdks/OneSignalSDK.js" async=""></script>
+  <script>
+    window.OneSignal = window.OneSignal || [];
+    OneSignal.push(function() {
+      OneSignal.init({
+        appId: "90c57079-9ef2-4719-bddf-361e6510de17",
+        googleProjectNumber: "256841700684"
+      });
+    });
+  </script>                     
+</html>
