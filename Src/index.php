955a86a373d188057bb9fd923006ef545d7782a5
Guilherme Branco Stracini <guilherme@guilhermebranco.com.br>
2024-07-14 19:28:03 +0100

    Update index.php (#52)
    * Update index.php
    
    * style: format code with PHP CS Fixer
    
    This commit fixes the style issues introduced in 4a89a40 according to the output
    from PHP CS Fixer.
    
    Details: https://github.com/guibranco/ZeroCool/pull/52
    
    * Update index.php
    
    * Update index.php
    
    ---------
    
    Co-authored-by: deepsource-autofix[bot] <62050782+deepsource-autofix[bot]@users.noreply.github.com>
    Co-authored-by: gstraccini[bot] <150967461+gstraccini[bot]@users.noreply.github.com>

--- Src/index.php
+++ Src/index.php
@@ -1,33 +1,35 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xmlns:fb="http://www.facebook.com/2008/fbml">
-<head>
-<meta property="og:title" content="Guilherme Branco Stracini - Portfolio" />
-<meta property="og:description" content="My old portfolio from 2007" /> 
-<meta property="og:type" content="website" />
-<meta property="og:url" content="https://zerocool.com.br/" />
-<meta property="og:image" content="https://zerocool.com.br/portfolio/imagens/icone.png" />
-<meta property="og:site_name" content="ZeroCool" />
-<meta property="fb:admins" content="1614826774" />
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<base href="https://zerocool.com.br/portfolio/" />
-<title>Guilherme Branco Stracini - Portfolio</title>
-<link rel="stylesheet" type="text/css" href="scripts/estilos.css?update=201210020000"/>
-<script type="text/javascript" src="https://platform.twitter.com/widgets.js"></script>
-<script type="text/javascript">
-  var _gaq = _gaq || [];
-  _gaq.push(['_setAccount', 'UA-2816366-13']);
-  _gaq.push(['_trackPageview']);
-
-  (function() {
-    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-  })();
-</script>
-
-</head>
-
-<body>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+	<title>Guilherme Branco Stracini - Portfolio</title>
+	<meta charset="utf-8" />
+    	<meta http-equiv="x-ua-compatible" content="ie=edge" />
+    	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="description" content="Portfolio of Guilherme Branco Stracini, senior software engineer, with professional life, experiences, skills, hobbies, and contact information." />
+	<meta property="og:url" content="https://zerocool.com.br/" />
+	<meta property="og:title" content="Guilherme Branco Stracini - Senior Software Engineer" />
+	<meta property="og:site_name" content="Guilherme Branco Stracini - Senior Software Engineer" />
+	<meta property="og:description" content="Portfolio of Guilherme Branco Stracini, senior software engineer, with professional life, experiences, skills, hobbies, and contact information." />
+	<meta property="og:image" content="https://zerocool.com.br/portfolio/imagens/icone.png" />
+	<meta property="og:image:type" content="image/png" />
+    	<meta property="og:image:width" content="250" />
+    	<meta property="og:image:height" content="250" />
+    	<meta property="og:type" content="website" />
+ 	<meta property="fb:app_id" content="290252964970555" />
+	<meta property="fb:admins" content="1614826774" />
+	<base href="https://zerocool.com.br/portfolio/" />
+	<link rel="stylesheet" type="text/css" href="scripts/estilos.css?update=<?php echo filemtime("scripts/estilos.css"); ?>"/>
+	<script type="text/javascript" src="https://platform.twitter.com/widgets.js"></script>
+	<script async src="https://www.googletagmanager.com/gtag/js?id=G-4KQXFWPLV8"></script>
+	<script>
+	  window.dataLayer = window.dataLayer || [];
+	  function gtag(){dataLayer.push(arguments);}
+	  gtag('js', new Date());
+	
+	  gtag('config', 'G-4KQXFWPLV8');
+	</script>
+    </head>
+    <body>
 	<div id="fb-root"></div>
 	<script>(function(d, s, id) {
 	  var js, fjs = d.getElementsByTagName(s)[0];
@@ -56,8 +58,8 @@
 						</a>
 					</li>
 					<li>
-						<a href="https://www.youtube.com/user/coolzerocoolzerocool" title="Canal coolzerocoolzerocool no Youtube" target="_blank">
-							<img src="imagens/youtube.png" width="24" height="24" alt="Canal coolzerocoolzerocool no Youtube" />
+						<a href="https://www.youtube.com/@GuilhermeBrancoStracini" title="Canal @GuilhermeBrancoStracini no Youtube" target="_blank">
+							<img src="imagens/youtube.png" width="24" height="24" alt="Canal @GuilhermeBrancoStracini no Youtube" />
 						</a>
 					</li>
 					<li>
@@ -71,7 +73,7 @@
 						</a>
 					</li>
 					<li>
-						<a href="https://www.linkedin.com/e/fpf/138350470" title="Guilherme Branco Stracini no LinkedIn" target="_blank">
+						<a href="https://www.linkedin.com/in/guilhermestracini/" title="Guilherme Branco Stracini no LinkedIn" target="_blank">
 							<img src="imagens/linkedin.png" width="24" height="24" alt="Guilherme Branco Stracini no LinkedIn" />
 						</a>
 					</li>
@@ -85,15 +87,15 @@
 			<div class="links">
 			<?php
                             $baseDir = "/home/zerocool/public_html/";
-			$o = opendir($baseDir);
-			$proibidos = array("cgi-bin","inovacao","portfolio", "static",".htpasswds",".well-known");
-			while($item = readdir($o)) {
-			    if(is_dir($baseDir.$item) && $item != "." && $item != ".." && !in_array($item, $proibidos)) {
-			        echo "<a href='https://guilhermebranco.com.br/".$item."/?utm_source=zerocool&utm_medium=projetos&utm_campaign=old_portfolio'>".ucwords(str_replace("_", " ", $item))."</a><br />";
-			    }
-			}
-			closedir($o);
-			?>
+	$o = opendir($baseDir);
+	$proibidos = array("cgi-bin","inovacao","portfolio", "static",".htpasswds",".well-known");
+	while($item = readdir($o)) {
+	    if(is_dir($baseDir.$item) && $item != "." && $item != ".." && !in_array($item, $proibidos)) {
+	        echo "<a href='https://guilhermebranco.com.br/".$item."/?utm_source=zerocool&utm_medium=projetos&utm_campaign=old_portfolio'>".ucwords(str_replace("_", " ", $item))."</a><br />";
+	    }
+	}
+	closedir($o);
+	?>
 			</div>
 		</div>
 
@@ -107,13 +109,13 @@
 	</div>
 
 	<script type="text/javascript">
-	window.___gcfg = {lang: 'pt-BR'};
-	  (function() {
-		var po = document.createElement('script'); po.type = 'text/javascript'; po.async = true;
-		po.src = 'https://apis.google.com/js/plusone.js';
-		var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s);
-	  })();
+		window.___gcfg = {lang: 'pt-BR'};
+		  (function() {
+			var po = document.createElement('script'); po.type = 'text/javascript'; po.async = true;
+			po.src = 'https://apis.google.com/js/plusone.js';
+			var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s);
+		  })();
 	</script>
 	<script type="text/javascript" src="https://assets.pinterest.com/js/pinit.js"></script>
-</body>
+    </body>
 </html>
