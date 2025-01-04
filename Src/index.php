68736bf6bc1bb181d992e39d9c1171028030c99c
Guilherme Branco Stracini <guilherme@guilhermebranco.com.br>
2025-01-04 14:55:30 +0000

    Update index.php

--- Src/index.php
+++ Src/index.php
@@ -28,7 +28,8 @@ $socialLinks = array(
     "linkedin" => "https://www.linkedin.com/in/guilhermestracini",
     "stackoverflow" => "https://stackoverflow.com/users/1890220/guilherme-branco-stracini",
     "instagram" => "https://www.instagram.com/gui.stracini",
-    "whatsapp" => "https://api.whatsapp.com/send/?phone=5511972659788&text=Hello%2C+Guilherme%21"
+    "whatsapp" => "https://api.whatsapp.com/send/?phone=5511972659788&text=Hello%2C+Guilherme%21",
+    "Wordpress" => "https://blog.guilhermebranco.com.br"
 );
 ksort($socialLinks);
 
